490340bbaa8f28ac1a94562058458ea788fb47e6
EECKHOUT Rémi <remieeckhout9@gmail.com>
2026-05-11 08:37:56 +0200

    hotfix: added IsGranted for ROLE_USER

--- src/Controller/MonPoids/BmiController.php
+++ src/Controller/MonPoids/BmiController.php
@@ -11,10 +11,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
 use Symfony\UX\Chartjs\Model\Chart;
 
-#[Route('/monpoids/bmi', name: 'app_MonPoids_bmi_')]
+#[Route('/monpoids/bmi', name: 'app_MonPoids_bmi_'), IsGranted("ROLE_USER")]
 final class BmiController extends AbstractController
 {
     #[Route('/', name: 'index', methods: ['GET'])]
