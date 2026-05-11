492865316ee47daac193d760acf84a88489c740a
EECKHOUT Rémi <remieeckhout9@gmail.com>
2026-05-11 15:33:11 +0200

    feat: add admin panel for recipes

---
+++ src/Form/MaCuisine/RecipeAdminType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form\MaCuisine;
+
+use App\Entity\MaCuisine\Recipe;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RecipeAdminType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('updatedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('name')
+            ->add('description')
+            ->add('author', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Recipe::class,
+        ]);
+    }
+}
