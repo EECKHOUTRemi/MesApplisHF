a5f2d391d6e69b32274e4161204494fdd9e0873d
EECKHOUT Rémi <remieeckhout9@gmail.com>
2026-05-14 17:12:14 +0200

    fix: specify fields types

--- src/Form/macuisine/RecipeAdminType.php
+++ src/Form/macuisine/RecipeAdminType.php
@@ -4,8 +4,11 @@ namespace App\Form\macuisine;
 
 use App\Entity\macuisine\Recipe;
 use App\Entity\User;
+use Doctrine\DBAL\Types\DateTimeType;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,14 +17,14 @@ class RecipeAdminType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('createdAt', null, [
+            ->add('createdAt', DateTimeType::class, [
                 'widget' => 'single_text',
             ])
-            ->add('updatedAt', null, [
+            ->add('updatedAt', DateTimeType::class, [
                 'widget' => 'single_text',
             ])
-            ->add('name')
-            ->add('description')
+            ->add('name', TextType::class)
+            ->add('description', TextareaType::class)
             ->add('author', EntityType::class, [
                 'class' => User::class,
                 'choice_label' => 'id',
