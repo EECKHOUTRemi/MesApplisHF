3c099d71d5391474e16633cb73b02b240efb8ffd
EECKHOUT Rémi <remieeckhout9@gmail.com>
2026-05-19 11:31:50 +0200

    feat: category and utensil admin panels

---
+++ src/Controller/admin/MaCuisine/CategoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller\admin\MaCuisine;
+
+use App\Entity\MaCuisine\Category;
+use App\Form\MaCuisine\CategoryType;
+use App\Repository\MaCuisine\CategoryRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin/ma/cuisine/category')]
+final class CategoryController extends AbstractController
+{
+    #[Route(name: 'app_admin_ma_cuisine_category_index', methods: ['GET'])]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('admin/MaCuisine/category/index.html.twig', [
+            'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_admin_ma_cuisine_category_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category->setCreatedAt(new DateTimeImmutable);
+            $category->setCreatedBy($this->getUser());
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_admin_ma_cuisine_category_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin/MaCuisine/category/new.html.twig', [
+            'category' => $category,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_ma_cuisine_category_show', methods: ['GET'])]
+    public function show(Category $category): Response
+    {
+        return $this->render('admin/MaCuisine/category/show.html.twig', [
+            'category' => $category,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_admin_ma_cuisine_category_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Category $category, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(CategoryType::class, $category);
+        $category->setUpdatedAt(new DateTimeImmutable());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_admin_ma_cuisine_category_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin/MaCuisine/category/edit.html.twig', [
+            'category' => $category,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_ma_cuisine_category_delete', methods: ['POST'])]
+    public function delete(Request $request, Category $category, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($category);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_admin_ma_cuisine_category_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Controller/admin/MaCuisine/UtensilController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller\admin\MaCuisine;
+
+use App\Entity\MaCuisine\Utensil;
+use App\Form\MaCuisine\UtensilType;
+use App\Repository\MaCuisine\UtensilRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin/ma/cuisine/utensil')]
+final class UtensilController extends AbstractController
+{
+    #[Route(name: 'app_admin_ma_cuisine_utensil_index', methods: ['GET'])]
+    public function index(UtensilRepository $utensilRepository): Response
+    {
+        return $this->render('admin/MaCuisine/utensil/index.html.twig', [
+            'utensils' => $utensilRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_admin_ma_cuisine_utensil_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $utensil = new Utensil();
+        $form = $this->createForm(UtensilType::class, $utensil);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $utensil->setCreatedAt(new DateTimeImmutable);
+            $utensil->setCreatedBy($this->getUser());
+            $entityManager->persist($utensil);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_admin_ma_cuisine_utensil_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin/MaCuisine/utensil/new.html.twig', [
+            'utensil' => $utensil,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_ma_cuisine_utensil_show', methods: ['GET'])]
+    public function show(Utensil $utensil): Response
+    {
+        return $this->render('admin/MaCuisine/utensil/show.html.twig', [
+            'utensil' => $utensil,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_admin_ma_cuisine_utensil_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Utensil $utensil, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(UtensilType::class, $utensil);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $utensil->setUpdatedAt(new DateTimeImmutable);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_admin_ma_cuisine_utensil_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin/MaCuisine/utensil/edit.html.twig', [
+            'utensil' => $utensil,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_ma_cuisine_utensil_delete', methods: ['POST'])]
+    public function delete(Request $request, Utensil $utensil, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$utensil->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($utensil);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_admin_ma_cuisine_utensil_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
