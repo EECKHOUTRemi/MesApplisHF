8bdfe57253f8a4a8ef70ffd3331b18425ae5b107
EECKHOUT Rémi <remieeckhout9@gmail.com>
2026-06-29 15:06:49 +0200

    fix: the default page is now the brochure one

--- src/Controller/BrochureController.php
+++ src/Controller/BrochureController.php
@@ -10,25 +10,24 @@ use Symfony\Component\Routing\Attribute\Route;
  * Pages vitrine publiques présentant MesApplisHF et ses deux outils (MaCuisine, MonPoids).
  * Accessibles sans authentification : point d'entrée pour les visiteurs avant inscription.
  */
-#[Route('/', name: 'app_brochure_', methods: ['GET'])]
 final class BrochureController extends AbstractController
 {
     /** @return Response */
-    #[Route(name: 'index', methods: ['GET'])]
+    #[Route('/', name: 'app_brochure_index', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('brochure/index.html.twig');
     }
 
     /** @return Response */
-    #[Route('macuisine', name: 'macuisine', methods: ['GET'])]
+    #[Route('/discover/macuisine', name: 'app_brochure_macuisine', methods: ['GET'])]
     public function maCuisine(): Response
     {
         return $this->render('brochure/macuisine.html.twig');
     }
 
     /** @return Response */
-    #[Route('monpoids', name: 'monpoids', methods: ['GET'])]
+    #[Route('/discover/monpoids', name: 'app_brochure_monpoids', methods: ['GET'])]
     public function monPoids(): Response
     {
         return $this->render('brochure/monpoids.html.twig');
