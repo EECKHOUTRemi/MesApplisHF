945d4f5125691b015b9bb9168c211f2e251abbbb
EECKHOUT Rémi <remieeckhout9@gmail.com>
2026-06-29 13:22:43 +0200

    feat: add admin panel for recipe images

--- src/Controller/admin/MaCuisine/ImagesController.php
+++ src/Controller/admin/MaCuisine/ImagesController.php
@@ -7,8 +7,9 @@ use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Filesystem\Filesystem;
 
-#[Route('/admin/macuisine/images'),
+#[Route('/admin/macuisine/images', name: 'app_admin_macuisine_images_'),
 IsGranted('ROLE_ADMIN')]
 /** Galerie admin de toutes les images stockées dans public/uploads/recipes. */
 final class ImagesController extends AbstractController
@@ -17,7 +18,7 @@ final class ImagesController extends AbstractController
      * @param string $recipesImagesDirectory Chemin absolu du dossier des images de recettes
      * @return Response
      */
-    #[Route(name: 'app_admin_macuisine_images_index', methods: ['GET'])]
+    #[Route(name: 'index', methods: ['GET'])]
     public function index(
         #[Autowire(param: 'recipes_images_directory')] string $recipesImagesDirectory,
     ): Response {
@@ -34,5 +35,30 @@ final class ImagesController extends AbstractController
         return $this->render('admin/MaCuisine/images/index.html.twig', [
             'images' => $images,
         ]);
+    }
+
+    /**
+     * @param string $imageToDelete Nom du fichier image à supprimer
+     * @param string $recipesImagesDirectory Chemin absolu du dossier des images de recettes
+     * @param Filesystem $filesystem
+     * @return Response
+     */
+    #[Route('/delete/{imageToDelete}', name: 'delete', methods: ['GET'])]
+    public function delete(
+        string $imageToDelete,
+        #[Autowire(param: 'recipes_images_directory')] string $recipesImagesDirectory,
+        Filesystem $filesystem,
+    ): Response {
+        $filename = basename($imageToDelete);
+        $path = $recipesImagesDirectory . '/' . $filename;
+
+        if (is_file($path)) {
+            $filesystem->remove($path);
+            $this->addFlash('success', sprintf('Image « %s » supprimée.', $filename));
+        } else {
+            $this->addFlash('danger', sprintf('Image « %s » introuvable.', $filename));
+        }
+
+        return $this->redirectToRoute('app_admin_macuisine_images_index');
     }
 }
