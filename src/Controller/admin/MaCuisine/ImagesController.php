f6cbc48e15c7b30086b6d68d7e1b2837f043619d
EECKHOUT Rémi <remieeckhout9@gmail.com>
2026-08-06 10:31:02 +0200

    fix: add CSRF token check and change from link to form for the delete image

--- src/Controller/admin/MaCuisine/ImagesController.php
+++ src/Controller/admin/MaCuisine/ImagesController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Filesystem\Exception\FileNotFoundException;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -40,18 +41,18 @@ final class ImagesController extends AbstractController
     }
 
     /**
+     * @param Request $request La requête HTTP courante
      * @param string $imageToDelete Nom du fichier image à supprimer
-     * @param string $recipesImagesDirectory Chemin absolu du dossier des images de recettes
-     * @param Filesystem $filesystem
      * @return Response
      */
-    #[Route('/delete/{imageToDelete}', name: 'delete', methods: ['GET'])]
-    public function delete(
-        string $imageToDelete,
-        #[Autowire(param: 'recipes_images_directory')] string $recipesImagesDirectory,
-        Filesystem $filesystem,
-        ImageHandler $imgHandler
-    ): Response {
+    #[Route('/delete/{imageToDelete}', name: 'delete', methods: ['POST'])]
+    public function delete(Request $request, string $imageToDelete, ImageHandler $imgHandler): Response 
+    {
+        if (!$this->isCsrfTokenValid('delete-image', $request->request->get('_token'))) {
+            $this->addFlash('danger', 'Jeton CSRF invalide.');
+            return $this->redirectToRoute('app_admin_macuisine_images_index');
+        }
+
         $filename = basename($imageToDelete);
 
         try {
