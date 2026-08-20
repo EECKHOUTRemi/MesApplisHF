bcc00a915560d2b3c17088a3dd03f42dfa31d102
EECKHOUTRemi <remieeckhout9@gmail.com>
2026-08-20 11:41:14 +0200

    feat: make username unique

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -17,7 +17,9 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Un compte avec la meme adresse mail existe déjà.')]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_USERNAME', fields: ['username'])]
+#[UniqueEntity(fields: ['username'], message: 'Un compte avec le meme nom d\'utilisateur existe déjà.')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
